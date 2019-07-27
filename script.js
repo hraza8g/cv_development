3b6e1d36031b20e29a4da0a28cbe9ef120ba7cb0
Hassan Raza <hassaninside@gmail.com>
2019-07-27 23:08:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const list = document.querySelector('#book-list ul');
+const forms = document.forms;
+
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
